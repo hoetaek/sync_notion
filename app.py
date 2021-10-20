a98d4d1a80ba07e04e2ce528e3a28f5e97f7cdd7
unknown <hoetaek@student.snue.ac.kr>
2021-10-20 15:52:12 +0900

    todoist url

--- app.py
+++ app.py
@@ -16,5 +16,15 @@ def hello_world():
     return "<script>window.onload = window.close();</script>"
 
 
+@app.route('/todoist')
+def todoist():
+    heavy_process = Process(  # Create a daemonic process with heavy "my_func"
+        target=update_notion_stocks,
+        daemon=True
+    )
+    heavy_process.start()
+    return "<script>window.onload = window.close();</script>"
+
+
 if __name__ == '__main__':
     app.run(debug=False)
