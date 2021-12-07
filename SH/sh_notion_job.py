51c6f12c9a135208544d24fb56b81acf9fa88f40
hoetaek <hoetaek@student.snue.ac.kr>
2021-12-07 18:15:25 +0900

    importing problem solved

--- SH/sh_notion_job.py
+++ SH/sh_notion_job.py
@@ -3,14 +3,14 @@ from os import environ
 
 from notion_client import Client
 
-from constants import sh_gtd_database_id
+from SH.constants import sh_gtd_database_id
 
 token = environ["NOTION_TOKEN_SH"]
 notion = Client(auth=token)
 
 
 ################ gtd ################
-def get_gtd_checked_collection_pages():
+def get_sh_gtd_checked_collection_pages():
     result = notion.databases.query(
         sh_gtd_database_id,
         filter={
@@ -41,4 +41,4 @@ if __name__ == "__main__":
     from pprint import pprint
 
     # result = notion.databases.retrieve(database_id= sh_gtd_database_id)
-    pprint(get_gtd_checked_collection_pages())
+    pprint(get_sh_gtd_checked_collection_pages())
--- SH/util.py
+++ SH/util.py
@@ -1,21 +1,23 @@
 import traceback
 
-import notion_job
+import SH.sh_notion_job as sh_notion_job
 
 
 def notion_cleanup_SH():
     try:
+        print("trying")
         update_checked_collection2done()
     except Exception:
-        notion_job.create_errorpage_in_gtd_collect(traceback.format_exc())
+        sh_notion_job.create_errorpage_in_gtd_collect(traceback.format_exc())
 
 
 def update_checked_collection2done():
-    page_results = notion_job.get_gtd_checked_collection_pages()
+    page_results = sh_notion_job.get_sh_gtd_checked_collection_pages()
+    print(page_results)
     for page in page_results:
         page_id = page["id"]
-        notion_job.update_gtd_page_complete(page_id)
+        sh_notion_job.update_gtd_page_complete(page_id)
 
 
 if __name__ == "__main__":
-    update_checked_collection2done()
+    notion_cleanup_SH()
