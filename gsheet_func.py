3a57c662fbf245367beac76b499108ff5bf9ac5d
라회택 <protaek@lahoetaeg-ui-MacBookPro-2.local>
2022-04-23 22:04:53 +0900

    refactor: new gsheet api key

--- gsheet_func.py
+++ gsheet_func.py
@@ -3,16 +3,16 @@ from os import environ
 import gspread
 
 credentials = {
-    "type": "service_account",
-    "project_id": "gsheet-notion-stock",
-    "private_key_id": environ["PRIVATE_KEY_ID"],
-    "private_key": environ["PRIVATE_KEY"].replace("\\n", "\n"),
-    "client_email": "get-stock-info@gsheet-notion-stock.iam.gserviceaccount.com",
-    "client_id": "113516897813603369913",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/get-stock-info%40gsheet-notion-stock.iam.gserviceaccount.com",
+  "type": "service_account",
+  "project_id": "gsheet-notion-stock",
+  "private_key_id": environ["PRIVATE_KEY_ID"],
+  "private_key": environ["PRIVATE_KEY"].replace("\\n", "\n"),
+  "client_email": "get-stock-info@gsheet-notion-stock.iam.gserviceaccount.com",
+  "client_id": "113516897813603369913",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/get-stock-info%40gsheet-notion-stock.iam.gserviceaccount.com"
 }
 
 
